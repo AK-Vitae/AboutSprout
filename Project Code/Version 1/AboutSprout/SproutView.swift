48403ee7d04950df76537b8a2a2bf6b3b65e884a
AK <ak.ramadugu@gmail.com>
2020-09-07 12:10:48 -0400

    Restricted app orientation and made fully compatible with all iOS 13 supporting devices.

--- Project Code/Version 1/AboutSprout/SproutView.swift
+++ Project Code/Version 1/AboutSprout/SproutView.swift
@@ -11,53 +11,56 @@ import SwiftUI
 struct SproutView: View {
     let sprout: Sprout
     var body: some View {
-        GeometryReader { geometry in
-            VStack (alignment: .center, spacing: 30){
-                HStack{
-                    Image(self.sprout.image)
-                        .renderingMode(.original)
-                        .resizable()
-                        .frame(maxWidth: geometry.size.width*0.25, maxHeight: geometry.size.height*0.15)
-                        .scaledToFill()
-                        .clipShape(RoundedRectangle(cornerRadius: 25))
-                        .shadow(color: Color.black, radius: 4, x: 0, y: 0)
-                        .padding(.leading)
-                        .padding(.top)
-                        .padding(.bottom)
-                    VStack{
-                        Text(self.sprout.displayName)
-                            .fontWeight(.bold)
-                            .font(.headline)
-                        Text("\(self.sprout.nomenclature)")
-                            .font(.subheadline)
-                            .italic()
-                            .fixedSize()
-                        
+        ScrollView{
+            GeometryReader { geometry in
+                VStack (alignment: .center, spacing: 30){
+                    HStack{
+                        Image(self.sprout.image)
+                            .renderingMode(.original)
+                            .resizable()
+                            //.frame(maxWidth: geometry.size.width*0.25, maxHeight: geometry.size.height)
+                            .frame(maxWidth: 100, maxHeight: 100)
+                            .scaledToFill()
+                            .clipShape(RoundedRectangle(cornerRadius: 25))
+                            .shadow(color: Color.black, radius: 4, x: 0, y: 0)
+                            .padding(.leading)
+                            .padding(.top)
+                            .padding(.bottom)
+                        VStack{
+                            Text(self.sprout.displayName)
+                                .fontWeight(.bold)
+                                .font(.headline)
+                            Text("\(self.sprout.nomenclature)")
+                                .font(.subheadline)
+                                .italic()
+                                .fixedSize()
+                            
+                        }
+                        .padding()
+                        .fixedSize(horizontal: false, vertical: true)
                     }
-                    .padding()
-                    .fixedSize(horizontal: false, vertical: true)
-                }
-                .background(Color("sproutGreen"))
-                .cornerRadius(8)
-                .shadow(color: .gray,radius: 4, x: 0, y: 0)
-                .padding(.leading, 10)
-                .padding(.trailing, 10)
-                Text(self.sprout.description)
-                    .bold()
-                    
-                    .padding()
-                    .fixedSize(horizontal: false, vertical: true)
-                    .multilineTextAlignment(.center)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color("sproutGreen"), lineWidth: 5)
-                            .shadow(color: .gray,radius: 4, x: 0, y: 0)
-                )
+                    .background(Color("sproutGreen"))
                     .cornerRadius(8)
+                    .shadow(color: .gray,radius: 4, x: 0, y: 0)
                     .padding(.leading, 10)
                     .padding(.trailing, 10)
-                
-                
+                    Text(self.sprout.description)
+                        .bold()
+                        
+                        .padding()
+                        .fixedSize(horizontal: false, vertical: true)
+                        .multilineTextAlignment(.center)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color("sproutGreen"), lineWidth: 5)
+                                .shadow(color: .gray,radius: 4, x: 0, y: 0)
+                    )
+                        .cornerRadius(8)
+                        .padding(.leading, 10)
+                        .padding(.trailing, 10)
+                    
+                    
+                }
             }
         }
     }
